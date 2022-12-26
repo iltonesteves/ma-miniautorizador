32b1542bda8b5ddd6511800c0a8ff313080baab6
iltonesteves <iltonesteves@hotmail.com>
2022-12-26 09:33:48 -0300

    ma-cartao criado

---
+++ src/main/java/com/api/macartao/MaCartaoApplication.java
@@ -0,0 +1,13 @@
+package com.api.macartao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaCartaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaCartaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/macartao/MaCartaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.macartao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaCartaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
