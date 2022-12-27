4b23f91aaa4b7986aa16a6f5d01bbae2d4543a88
iltonesteves <iltonesteves@hotmail.com>
2022-12-27 10:58:17 -0300

    ma-transacao criado

---
+++ ma-transacao/src/main/java/com/api/matransacao/MaTransacaoApplication.java
@@ -0,0 +1,13 @@
+package com.api.matransacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaTransacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaTransacaoApplication.class, args);
+	}
+
+}
