a3fc3014f01ed9cbd704da99d10a3f6e449c88e5
iltonesteves <iltonesteves@hotmail.com>
2023-01-02 15:33:35 -0300

    Implementando gatway e configurando Zuul

---
+++ ma-api-gateway-zuul/src/test/java/com/api/gatewayzuul/MaApiGatewayZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.gatewayzuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaApiGatewayZuulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
