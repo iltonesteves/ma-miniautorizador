9d37a9bc0554e813d13394c20277c05480e07390
iltonesteves <iltonesteves@hotmail.com>
2023-01-03 16:18:34 -0300

    configuração do projeto ma-oauth

--- ma-oauth/src/main/java/com/api/maoauth/MaOauthApplication.java
+++ ma-oauth/src/main/java/com/api/maoauth/MaOauthApplication.java
@@ -2,7 +2,10 @@ package com.api.maoauth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+
+@EnableEurekaClient
 @SpringBootApplication
 public class MaOauthApplication {
 
