60670dbc4ba4145b86f443b449ba4ffe1be076c5
iltonesteves <iltonesteves@hotmail.com>
2023-01-03 08:53:41 -0300

    projeto ma-user criado

--- ma-transacao/src/main/java/com/api/matransacao/resources/TransacaoResource.java
+++ ma-transacao/src/main/java/com/api/matransacao/resources/TransacaoResource.java
@@ -1,6 +1,9 @@
 package com.api.matransacao.resources;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,12 +17,16 @@ import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 @RequestMapping("/transacoes")
 public class TransacaoResource {
 
+	private Logger log = LoggerFactory.getLogger(TransacaoResource.class);
 	@Autowired
 	private TransacaoService service;
+	@Autowired
+	private Environment env;
 
 	@HystrixCommand(fallbackMethod = "efetivarTransacaoAlternativo")
 	@PostMapping
 	public String efetivarTransacao(@RequestBody Transacao obj) {
+		log.info("Porta " + env.getProperty("local.server.port"));
 		String status = service.efetivarTransacao(obj);
 		return status;
 	}
