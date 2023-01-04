755094533ac86443671577474a97fabd22399426
iltonesteves <iltonesteves@hotmail.com>
2023-01-04 10:49:58 -0300

    Zuul autorizando

---
+++ ma-api-gateway-zuul/src/main/java/com/api/gatewayzuul/config/ResouceServerConfig.java
@@ -0,0 +1,40 @@
+package com.api.gatewayzuul.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+
+@Configuration
+@EnableResourceServer
+public class ResouceServerConfig extends ResourceServerConfigurerAdapter {
+
+	@Autowired
+	private JwtTokenStore tokenStore;
+	
+	// DEFINIÇÃO DE ROTAS
+	private static final String[] PUBLIC = {"/ma-oauth/oauth/token"};
+	private static final String[] OPERATOR = {"/ma-cartao/**"}; // para o operador qualquer rota está autorizado
+	private static final String[] ADMIN = {"/ma-transacao/**","/ma-user/**"}; // para o admin
+	
+	@Override
+	public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
+		resources.tokenStore(tokenStore);
+		}
+
+	@Override
+	public void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+		.antMatchers(PUBLIC).permitAll()
+		.antMatchers(HttpMethod.GET, OPERATOR).hasAnyRole("OPERATOR","ADMIN")
+		.antMatchers(ADMIN).hasRole("ADMIN")
+		.anyRequest().authenticated();
+
+	
+	}
+
+}
