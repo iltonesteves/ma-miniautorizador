d75fc9399345139031f70fc8b45f28e9342a3189
iltonesteves <iltonesteves@hotmail.com>
2023-01-04 13:23:32 -0300

    Configuração do Cors

--- ma-api-gateway-zuul/src/main/java/com/api/gatewayzuul/config/ResouceServerConfig.java
+++ ma-api-gateway-zuul/src/main/java/com/api/gatewayzuul/config/ResouceServerConfig.java
@@ -1,13 +1,22 @@
 package com.api.gatewayzuul.config;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 @EnableResourceServer
@@ -15,26 +24,44 @@ public class ResouceServerConfig extends ResourceServerConfigurerAdapter {
 
 	@Autowired
 	private JwtTokenStore tokenStore;
-	
+
 	// DEFINIÇÃO DE ROTAS
-	private static final String[] PUBLIC = {"/ma-oauth/oauth/token"};
-	private static final String[] OPERATOR = {"/ma-cartao/**"}; // para o operador qualquer rota está autorizado
-	private static final String[] ADMIN = {"/ma-transacao/**","/ma-user/**","/actuator/refresh","/ma-cartao/actuator/**","/ma-oauth/actuator/**"}; // para o admin
-	
+	private static final String[] PUBLIC = { "/ma-oauth/oauth/token" };
+	private static final String[] OPERATOR = { "/ma-cartao/**" }; // para o operador qualquer rota está autorizado
+	private static final String[] ADMIN = { "/ma-transacao/**", "/ma-user/**", "/actuator/refresh",
+			"/ma-cartao/actuator/**", "/ma-oauth/actuator/**" }; // para o admin
+
 	@Override
 	public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
 		resources.tokenStore(tokenStore);
-		}
+	}
 
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests()
-		.antMatchers(PUBLIC).permitAll()
-		.antMatchers(HttpMethod.GET, OPERATOR).hasAnyRole("OPERATOR","ADMIN")
-		.antMatchers(ADMIN).hasRole("ADMIN")
-		.anyRequest().authenticated();
-
-	
+		http.authorizeRequests().antMatchers(PUBLIC).permitAll().antMatchers(HttpMethod.GET, OPERATOR)
+				.hasAnyRole("OPERATOR", "ADMIN").antMatchers(ADMIN).hasRole("ADMIN").anyRequest().authenticated();
+		
+		http.cors().configurationSource(corsConfigurationSource());
 	}
 
+	@Bean
+	public CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration corsConfig = new CorsConfiguration();
+		corsConfig.setAllowedOrigins(Arrays.asList("*"));
+		corsConfig.setAllowedMethods(Arrays.asList("POST", "GET", "PUT", "DELETE", "PATH"));
+		corsConfig.setAllowCredentials(true);
+		corsConfig.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
+
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", corsConfig);
+
+		return source;
+	}
+
+	@Bean
+	public FilterRegistrationBean<CorsFilter> corsFilter(){
+		FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>(new CorsFilter(corsConfigurationSource()));
+		bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+		return bean;
+	}
 }
